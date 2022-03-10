db7033ae715debddf8f6d2b6eb1704aaa9812461
Abhishek Shukla <shukla1996abhishek@gmail.com>
2022-03-10 12:00:00 -0800

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 const usersRouter = require('./routes/users')
 
